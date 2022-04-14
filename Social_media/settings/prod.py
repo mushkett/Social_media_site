5cc32f5b66453b5bbc02e7ac6c9ac4625f8d83b2
mushkett <lebedka.dmitry@gmail.com>
2022-04-14 16:16:53 +0300

    deploy part 1

--- Social_media/settings/prod.py
+++ Social_media/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import django_on_heroku
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
@@ -9,3 +10,34 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'https://social-media-site-pet-project.herokuapp.com/'
 ]
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+# Heroku setting
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
