17a1fcdee8d2626756785e0d591face49196f3e4
Dmitry <92876864+mushkett@users.noreply.github.com>
2026-04-08 20:23:02 +0300

    Added caching (#3)

--- groups/urls.py
+++ groups/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 from groups import views
 
 app_name = 'groups'
@@ -7,7 +8,7 @@ app_name = 'groups'
 _SLUG = r'(?P<slug>[-\w]+)'
 
 urlpatterns = [
-    path('', views.ListGroups.as_view(), name='all'),
+    path('', cache_page(60 * 5)(views.ListGroups.as_view()), name='all'),
     path('new/', views.CreateGroup.as_view(), name='create'),
     re_path(r'^posts/in/' + _SLUG + r'$', views.SingleGroup.as_view(), name='single'),
     re_path(r'^join/' + _SLUG + r'/$', views.JoinGroup.as_view(), name='join'),
